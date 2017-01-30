5e7040151d4c8a5fcaec3631097eefdb86c83588
Zachary Bennett <zbennett10@hotmail.com>
2017-01-30 15:17:23 -0500

    configuring for heroku

--- bin/www.js
+++ bin/www.js
@@ -4,8 +4,8 @@
 
  var server = http.createServer(app);
 
- app.set('port', process.env.PORT || '3000');
- server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+ app.set('port', process.env.PORT || '80');
+ server.listen(process.env.PORT || 80, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log(`Image Search API listening at ${addr.address}: ${addr.port}`);
 });
