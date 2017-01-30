a294c51e6d8cebfa7c666ac1212906f6e838e349
Zachary Bennett <zbennett10@hotmail.com>
2017-01-30 14:56:35 -0500

    configuring for heroku deployment

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var http = require('http');
-var express = require('express');
+// var http = require('http');
+// var express = require('express');
 
-var app = express();
-var server = http.createServer(app);
-
-
-//set view engine
-app.set('view engine', 'pug');
-
-//middleware----------
-//serve static files from this directory
-// app.use('/assets', express.static(__dirname + '/public'));
-
-//configure routes
-var indexRoute = require('../routes/index');
-var apiRoute = require('../routes/api');
-
-app.use('/', indexRoute);
-app.use('/', apiRoute);
+// var app = express();
+// var server = http.createServer(app);
 
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log(`Image Search API listening at ${addr.address}: ${addr.port}`);
-});
+// //set view engine
+// app.set('view engine', 'pug');
+
+// //middleware----------
+// //serve static files from this directory
+// // app.use('/assets', express.static(__dirname + '/public'));
+
+// //configure routes
+// var indexRoute = require('../routes/index');
+// var apiRoute = require('../routes/api');
+
+// app.use('/', indexRoute);
+// app.use('/', apiRoute);
+
+
+// server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+//   var addr = server.address();
+//   console.log(`Image Search API listening at ${addr.address}: ${addr.port}`);
+// });
