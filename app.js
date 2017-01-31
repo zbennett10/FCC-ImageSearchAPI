e546fe05faf0f24e5187c7215a7574dce924ecab
Zachary Bennett <zbennett10@hotmail.com>
2017-01-31 10:14:02 -0500

    minor refactoring and added better handling of page errors

--- app.js
+++ app.js
@@ -12,12 +12,23 @@ app.set('view engine', 'pug');
 //serve static files from this directory
 app.use(express.static(__dirname + '/public'));
 
+//handle 404
+app.use(function(req,res) {
+  res.send('404: Page Not Found', 404);
+});
+
+//handle 500
+app.use(function(req,res) {
+  res.send('500: Internal Server Error', 500);
+});
+
 //configure routes
 var indexRoute = require('./routes/index');
 var apiRoute = require('./routes/api');
 
 app.use('/', indexRoute);
 app.use('/', apiRoute);
+
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,7 @@ var Search = require('../models/search.js');
 
 //search endpoint
 router.get('/api/search/:searchStr', function(req, res, next) {
-    if(req.query.offset && (/^\d+$/g).test(req.query.offset)) {     //check if an offset was requested
-        getImagesOffset(req,res);
-    } else {
-        getImages(req,res);      
-    } 
+    return req.query.offset && (/^\d+$/g).test(req.query.offset) ? getImages(req, res, req.query.offset) : getImages(req,res);
 });
 
 //history endpoint
@@ -23,7 +19,7 @@ router.get('/api/history', function(req,res) {
 //store search
 function saveSearch(search) {
     mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
-    var searchDocument = Search({
+    var searchDocument = Search({ //create search document based off of Search schema
         searchTerm: search,
         timestamp: moment().unix()
     });
@@ -40,12 +36,11 @@ function getHistory(res) {
     console.log('running history function');
     var history = [];
     mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
-    var query = Search.find({}).sort({timestamp: 'descending'}).limit(10);
+    var query = Search.find({}).sort({timestamp: 'descending'}).limit(10); //find and sort last ten searches
     query.exec(function(err,searches) {
         if(err) throw err;
-        console.log("History Found");
-        searches.forEach(search => history.push({searchTerm: '', timestamp: 0}));
-        for(var i = 0; i < searches.length; i++) {
+        searches.forEach(search => history.push({searchTerm: '', timestamp: 0})); //add empty object to history array for each search
+        for(var i = 0; i < searches.length; i++) {  //populate search objects with values
             history[i].searchTerm = searches[i].searchTerm;
             history[i].timestamp = searches[i].timestamp;
         }
@@ -54,28 +49,20 @@ function getHistory(res) {
     mongoose.connection.close();
 }
 
-function getImagesOffset(req,res) {
-    saveSearch(req.params.searchStr);
-    console.log('about to make request');
-    request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10&page=${req.query.offset}`, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.json(JSON.parse(body));
-        } else {
-            console.log(error);
-        }
-    });
-}
-
-function getImages(req,res) {
-    saveSearch(req.params.searchStr);
-    console.log('about to make request');
-    request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10`, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.json(JSON.parse(body));
-        } else {
-            console.log(error);
-        }
-    });
+//request images from pixabay
+function getImages(req,res,offset) {
+    saveSearch(req.params.searchStr); //save search to database
+    if(offset) {
+        request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10&page=${req.query.offset}`, 
+                function (error, response, body) {
+                    return !error && response.statusCode == 200 ? res.json(JSON.parse(body)) : console.log(error);
+                });
+    } else {
+         request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10`, 
+            function (error, response, body) {
+                return !error && response.statusCode == 200 ? res.json(JSON.parse(body)) : console.log(error);
+            });
+    }
 }
 
 
