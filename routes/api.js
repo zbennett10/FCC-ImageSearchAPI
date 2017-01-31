72369601546da2568be3725b219023817d8ddbe3
Zachary Bennett <zbennett10@hotmail.com>
2017-01-31 08:10:44 -0500

    configuring for heroku

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ router.get('/api/history', function(req,res) {
 
 //store search
 function saveSearch(search) {
-    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
+    mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
     var searchDocument = Search({
         searchTerm: search,
         timestamp: moment().unix()
@@ -39,7 +39,7 @@ function getHistory(res) {
     //example query to get every image
     console.log('running history function');
     var history = [];
-    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
+    mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds137759.mlab.com:37759/freecodecamp`);
     var query = Search.find({}).sort({timestamp: 'descending'}).limit(10);
     query.exec(function(err,searches) {
         if(err) throw err;
@@ -57,7 +57,7 @@ function getHistory(res) {
 function getImagesOffset(req,res) {
     saveSearch(req.params.searchStr);
     console.log('about to make request');
-    request(`https://pixabay.com/api/?key=${PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10&page=${req.query.offset}`, function (error, response, body) {
+    request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10&page=${req.query.offset}`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.json(JSON.parse(body));
         } else {
@@ -69,7 +69,7 @@ function getImagesOffset(req,res) {
 function getImages(req,res) {
     saveSearch(req.params.searchStr);
     console.log('about to make request');
-    request(`https://pixabay.com/api/?key=${PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10`, function (error, response, body) {
+    request(`https://pixabay.com/api/?key=${process.env.PIXKEY}&q=${req.params.searchStr}&image_type=photo&per_page=10`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.json(JSON.parse(body));
         } else {
