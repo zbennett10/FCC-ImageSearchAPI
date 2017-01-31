3cdbf9f5c313ceab53eb09298d4916475fa8c77a
Zachary Bennett <zbennett10@hotmail.com>
2017-01-31 08:52:26 -0500

    updating ui

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express();
 
 router.get('/', function(req, res) {
-    res.send('<html><head><link href=assets/index.css type=text/css rel=stylesheet></head><body><h1>Hello World!</h1></body></html>');
+    res.render('../views/index.pug', {title: 'Image Search API'});
 });
 
 module.exports = router;
